4bd1781ede5ea876a557182de89dd1bacfa7190d
John L Olsen <john@itcsolutions.com.au>
2020-10-20 01:28:06 +1100

    add volumes.sh

---
+++ volumes.sh
@@ -0,0 +1,6 @@
+for i in pgdata pgdata-replica2 postgres-config3 config-a-horse config-cheirrs config-cheirrs-oseer data-a-horse data-cheirrs data-cheirrs-oseer; do
+sudo mkdir -p /mnt/disk/$i
+sudo chmod 777 /mnt/disk/$i
+done
+
+
